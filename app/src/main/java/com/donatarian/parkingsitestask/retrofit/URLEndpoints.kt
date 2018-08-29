67c25ff2c9327be4744e93a5fa703066b9266213
donkii <donatkajtazi@gmail.com>
2018-08-29 17:04:32 +0200

    minor commit

---
+++ app/src/main/java/com/donatarian/parkingsitestask/retrofit/URLEndpoints.kt
@@ -0,0 +1,12 @@
+package com.donatarian.parkingsitestask.retrofit
+
+class URLEndpoints {
+
+    companion object {
+        //region GET
+
+        fun getParkingSite():String{
+            return Utils.ServerInfo.ServerURL("signature_ws/get_signature")
+        }
+    }
+}
